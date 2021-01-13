87486bd0889090fb5d87a780d9f6b5b5c0e9b00a
Your Name <you@example.com>
2021-01-13 15:44:35 +0530

    fc

---
+++ src/component/Sum.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react"
+
+class Sum extends Component {
+    state = {
+        result: 0,
+        num1: 0,
+        num2: 0,
+    }
+
+    handleNum1(e) {
+        console.log(e)
+        const num1 = (e.target.value)
+        this.setState(prevState => {
+            return {
+                num1: num1,
+                num2: prevState.num2,
+            }
+        })
+    }
+    handleNum2(e) {
+        console.log(e)
+        const num2 = (e.target.value)
+        this.setState(prevState => {
+            return {
+                num2: num2,
+                num1: prevState.num1,
+            }
+        })
+    }
+    handleSum(event) {
+
+        const num1 = this.state.num1;
+        const num2 = this.state.num2;
+
+        const result = parseInt(num1) + parseInt(num2);
+        this.setState({
+            'result': result
+        })
+
+
+
+    }
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col bprderClass">
+                        <input aria-label="num1-input" text="text" onChange={this.handleNum1.bind(this)} value={this.state.num1}></input>
+                        <input aria-label="num2-input" text="text" onChange={this.handleNum2.bind(this)} value={this.state.num2}></input>
+                    </div>
+                    <button aria-label="sum-button" onClick={this.handleSum.bind(this)}>Sum</button>
+                    <b aria-label="sum-output"  >{this.state.result}</b>
+                </div>
+            </div>
+        )
+    }
+}
+export default Sum
